f37a43d72315da5654a65ccec0d2ef71f3190e07
simtun <simonas.tunaitis@gmail.com>
2023-09-02 19:43:15 +0300

    Move activities to a new line when there is not enough space to display them in a single row

--- cmd/test.go
+++ cmd/test.go
@@ -2,11 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"stravastats/internal/service"
 	"stravastats/internal/ui"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 var testCmd = &cobra.Command{
@@ -21,18 +23,34 @@ var testCmd = &cobra.Command{
 			return err
 		}
 
-		var line []string
+		var ri = 0
+		var rw = 0
+		var rows [][]string = [][]string{[]string{}}
+
+		tw, _, err := terminal.GetSize(int(os.Stdin.Fd()))
+		if err != nil {
+			return err
+		}
 
 		for _, k := range activities {
 			if a, ok := stats.Activities[k]; ok {
 				b := ui.Box(a)
-				line = append(line, b)
+				rw = rw + lipgloss.Width(b)
+				if rw > tw {
+					rw = 0
+					rows = append(rows, []string{})
+					ri++
+				}
+
+				rows[ri] = append(rows[ri], b)
 			}
 		}
 
-		x := lipgloss.JoinHorizontal(lipgloss.Bottom, line...)
-
-		fmt.Println(x)
+		//x := lipgloss.JoinHorizontal(lipgloss.Bottom, line...)
+		for _, r := range rows {
+			x := lipgloss.JoinHorizontal(lipgloss.Bottom, r...)
+			fmt.Println(x)
+		}
 
 		return nil
 	},
